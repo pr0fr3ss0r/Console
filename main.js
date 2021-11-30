c286a2573b3e644fa47efa22faa04f0f425ebb96
pr0fr3ss0r <84141032+pr0fr3ss0r@users.noreply.github.com>
2021-11-29 19:12:36 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+
+var first_name = "Purno";
+console.log(first_name);
+
+var last_name = "Karmakar";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var length_of_full_name = full_name.length;
+console.log(length_of_full_name);
+
+var subtraction = Math.subtraction(9,5);
+
+var multiplication = Math.multiplication(7,8)
